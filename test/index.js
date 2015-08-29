132ff5fc63bb57eb5d0465e303f21f92b3d12ac1
bluespring <bluedarker@gmail.com>
2015-08-29 22:55:50 +0800

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,12 @@
+'use strict';
+var assert = require('assert');
+
+
+
+describe('showapi-sdk' , function () {
+  it('不用真实appId与secret的话没什么好测试的,请使用根目录下example_test.js自行测试!' , function () {
+    assert(true,'');
+  });
+
+});
+
